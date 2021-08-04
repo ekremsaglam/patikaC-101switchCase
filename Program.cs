cb513139e140785c54eb6afbb3f3119f13f0e6fe
Ekrem Sağlam <ekremsaglammm@gmail.com>
2021-08-04 07:57:33 +0300

    Second Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace switchCase
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int month =DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart");
+                    break;
+                case 8:
+                    Console.WriteLine("Ağustos");
+                    break;
+            }
+
+            switch (month){
+
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış ayındasınız");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz ayındasınız");
+                    break;
+                    
+
+            }
         }
     }
 }
